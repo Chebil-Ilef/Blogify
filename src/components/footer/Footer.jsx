bd9c2b0d74405f42f8e9183f3c013a90342a74d7
Chebil-Ilef <ilef.chebil@insat.ucar.tn>
2023-09-06 11:34:37 +0100

    home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
     <div className={styles.container}>
       <div>©2023 Ilef Chebil. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Ilef Dev Facebook Account" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Ilef Dev" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Ilef Dev" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Ilef Dev" />
       </div>
     </div>
   );
