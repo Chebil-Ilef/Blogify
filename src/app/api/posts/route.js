b08cbb68b3040deddb4c4f54467d4e0624092793
Chebil-Ilef <ilef.chebil@insat.ucar.tn>
2023-09-16 12:02:28 +0100

    delete and add posts

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -19,3 +19,20 @@ export const GET = async (req, res) => {
 
    
 }
+
+export const POST = async (request) => {
+    const body = await request.json();
+  
+    const newPost = new Post(body);
+  
+    try {
+      await connect();
+  
+      await newPost.save();
+  
+      return new NextResponse("Post has been created", { status: 201 });
+    } catch (err) {
+      return new NextResponse("Database Error", { status: 500 });
+    }
+  };
+
